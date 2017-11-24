ee0a42519211923826d654d9c042ee0fcf313a35
Florian <33514818+Acaledus@users.noreply.github.com>
2017-11-24 13:38:14 +0100

    Zweite Exception
    Jetzt hat alles eine Exception.

--- PSE_2017_03_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex03/p1/GeometryFunctions.java
+++ PSE_2017_03_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex03/p1/GeometryFunctions.java
@@ -3,30 +3,29 @@ package de.unistuttgart.iaas.pse.ex03.p1;
 
 public class GeometryFunctions {
 
-	/*
+	/**
 	 * Berechnet den Kreisumfang eines Kreises mit gegebenem Radius.
 	 * @param r Der Radius des Kreises.
 	 * @return Den Umfang des Kreises.
 	 */
 	public static double computeCircumference(double r) throws Exception {
 		if (r < 0) {
-
 			throw new Exception();
 		} // end of if
 		double a = 2 * Math.PI * r;
 		return a;
 	}
 
-	/*
+	/**
 	 * Berechnet die Hypotenuse eines rechtwinkligen Dreiecks mit gegebener
 	 * Kathete und Ankathete.
 	 * @param a Eine Seite des Dreiecks.
 	 * @param b Eine andere Seite des Dreiecks. 
 	 * @return Die Länge der dritten Seite.
 	 */
-	public static double pythagorenTheorem(int a, int b) {
+	public static double pythagorenTheorem(int a, int b) throws Exception{
 		if (a < 0 || b < 0) {
-			return -1;
+			throw new Exception();
 		} // end of if
 		double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 		return c;
@@ -44,6 +43,10 @@ public class GeometryFunctions {
 		} catch (Exception e) {
 			System.out.println("Es gibt keinen Kreis mit negativem Radius!");
 		}
-		System.out.println(pythagorenTheorem(2, 3));
+		try {
+			System.out.println(pythagorenTheorem(2, 3));
+		} catch (Exception e) {
+			System.out.println("Es gibt kein Dreieck mit negativen Seitenlängen!");
+		}
 	}
 }
